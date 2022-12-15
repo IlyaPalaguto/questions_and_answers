1d13334d25945d6bfe4253047de6b79f8cc855ff
Ilia <i.v.palaguto@gmail.com>
2022-12-15 15:59:41 +0500

    fix(create_answer_feat): Just authenticate user can create answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  before_action :authenticate_user!
   def create
     @answer = question.answers.build(answer_params)
     if @answer.save
--- spec/acceptance/answers/create_spec.rb
+++ spec/acceptance/answers/create_spec.rb
@@ -1,29 +1,43 @@
 require 'rails_helper'
 
-feature 'User can create answer', %q{
+feature 'Just authenticated user can create answer', %q{
   In order help to someone
   I'd like to be able write answers for a questuions
 } do
 
+  given(:user) { create(:user) }
   given(:question) { create(:question) }
 
-  background do
-    visit question_path(question)
+  describe 'Authenticated user' do
+    background do
+      login(user)
+      visit question_path(question)
+    end
+  
+    scenario 'create answer' do
+      fill_in 'Title', with: 'Answer for question'
+      fill_in 'Body', with: 'answer answer answer'
+      click_on 'Send'
+  
+      expect(page).to have_content 'Your answer successfully created'
+      expect(page).to have_content 'Answer for question'
+      expect(page).to have_content 'answer answer answer'
+    end
+  
+    scenario 'tries create answer with errors' do
+      click_on 'Send'
+  
+      expect(page).to have_content "Title can't be blank"
+    end
   end
 
-  scenario 'User create answer' do
+  scenario 'Unauthenticated user tries create answer' do
+    visit question_path(question)
+
     fill_in 'Title', with: 'Answer for question'
     fill_in 'Body', with: 'answer answer answer'
     click_on 'Send'
 
-    expect(page).to have_content 'Your answer successfully created'
-    expect(page).to have_content 'Answer for question'
-    expect(page).to have_content 'answer answer answer'
-  end
-
-  scenario 'User tries create answer with errors' do
-    click_on 'Send'
-
-    expect(page).to have_content "Title can't be blank"
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
